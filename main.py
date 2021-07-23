10146d3d19c934f1fcf83df1be56c35129949b28
Carlos <cadusouza14@gmail.com>
2021-07-23 15:02:36 -0300

    Implemented abstract classes to make the code more flexible

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from datetime import datetime, timedelta
 import asyncio
 import scrap
 import mail
 import smtplib
-import time
 import dotenv
 import aiohttp
 import json
@@ -18,39 +16,35 @@ async def main():
         "https://g1.globo.com/natureza/"
     ]
 
-    while True:
-        message = ""
-        async with aiohttp.ClientSession() as session:
+    message = ""
+    async with aiohttp.ClientSession() as session:
+        parser = scrap.G1Parser()
+        scrapper = scrap.G1Scrapper()
 
-            for rawData in await scrap.GetLatestG1News(session, urls):
-                for parsedData in scrap.ParseNews(rawData):
-                    message += mail.ParseNewsToEmailStr(
-                        parsedData, 0, 0, 2, "templates", "news.html"
-                    )
-
-            with (
-                smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp,
-                open("receivers.json", "r") as jsonFile
-            ):
-                sender = dotenv.dotenv_values(".env")
-                receivers = json.load(jsonFile)
-
-                emailMessage = mail.CreateMessage(
-                    sender["EMAIL"],
-                    ", ".join(receivers["emails"]),
-                    "Recent news", message
+        for rawData in await scrapper.ScrapNews(session, urls):
+            for parsedData in parser.ParseNews(rawData):
+                message += mail.ParseNewsToEmailStr(
+                    parsedData, 0, 0, 2, "templates", "news.html"
                 )
 
-                smtp.login(
-                    sender["EMAIL"],
-                    sender["PASSWORD"]
-                )
+        with (
+            smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp,
+            open("receivers.json", "r") as jsonFile
+        ):
+            receivers = json.load(jsonFile)
+            sender = dotenv.dotenv_values(".env")
+            emailMessage = mail.CreateMessage(
+                sender["EMAIL"],
+                ", ".join(receivers["emails"]),
+                "Recent news", message
+            )
 
-                smtp.send_message(emailMessage)
+            smtp.login(
+                sender["EMAIL"],
+                sender["PASSWORD"]
+            )
 
-            nextMessageTime = (datetime.now().replace(
-                hour=12, minute=0, second=0) + timedelta(1)) - datetime.now()
-            time.sleep(nextMessageTime.total_seconds())
+            smtp.send_message(emailMessage)
 
 
 loop = asyncio.get_event_loop()
--- scrap.py
+++ scrap.py
@@ -1,10 +1,12 @@
-import dataclasses
-from typing import Generator
-from bs4 import BeautifulSoup
-import aiohttp
-import datetime
+import abc
 import asyncio
+import dataclasses
+import datetime
 import json
+from typing import Generator, Iterable
+
+import aiohttp
+from bs4 import BeautifulSoup
 
 
 @dataclasses.dataclass
@@ -14,41 +16,57 @@ class NewsInfo:
     url: str
 
 
-async def GetLatestNews(session: aiohttp.ClientSession, urls: list[str]) -> list[dict]:
-    async def G1News(url: str) -> dict:
-        async with session.get(url) as response:
-            soup = BeautifulSoup(await response.text(), "html.parser")
+class AbstractNewsScrapper(abc.ABC):
 
-            news = soup.find("main")
-
-            newsGrid = news.find_all("div", recursive=False)[2]
-
-            info = newsGrid.find_all("div", recursive=False)[-1]
-            script = str(info.div.div.div.script)
-            jsonInfoStart = script.find('"config":') - 1
-            jsonInfoEnd = script.rfind(", {lazy")
-
-            newsRawData = script[jsonInfoStart:jsonInfoEnd]
-
-            return json.loads(newsRawData)
-
-    tasks = (asyncio.create_task(G1News(url)) for url in urls)
-
-    return await asyncio.gather(*tasks)
+    @abc.abstractmethod
+    async def ScrapNews(self, session: aiohttp.ClientSession, urls: Iterable[str]) -> tuple[dict]:
+        pass
 
 
-def ParseNews(newsRawData: dict) -> Generator[NewsInfo, None, None]:
-    for item in newsRawData["items"]:
-        date = item["created"].split("T")[0].split("-")
-        newsTime = datetime.date(int(date[0]), int(date[1]), int(date[2]))
-        today = datetime.date.today()
-        timeElapsed = today - newsTime
+class AbstractNewsParser(abc.ABC):
 
-        if timeElapsed.days > 1:
-            continue
+    @abc.abstractmethod
+    def ParseNews(self, rawdata: dict) -> Generator[NewsInfo, None, None]:
+        pass
 
-        yield NewsInfo(
-            title=item["content"]["title"],
-            summary=item["content"]["summary"],
-            url=item["content"]["url"]
-        )
+
+class G1Scrapper(AbstractNewsScrapper):
+    async def ScrapNews(self, session: aiohttp.ClientSession, urls: Iterable[str]) -> tuple[dict]:
+        async def G1News(url: str) -> dict:
+            async with session.get(url) as response:
+                soup = BeautifulSoup(await response.text(), "html.parser")
+
+                news = soup.find("main")
+
+                newsGrid = news.find_all("div", recursive=False)[2]
+
+                info = newsGrid.find_all("div", recursive=False)[-1]
+                script = str(info.div.div.div.script)
+                jsonInfoStart = script.find('"config":') - 1
+                jsonInfoEnd = script.rfind(", {lazy")
+
+                newsRawData = script[jsonInfoStart:jsonInfoEnd]
+
+                return json.loads(newsRawData)
+
+        tasks = (asyncio.create_task(G1News(url)) for url in urls)
+
+        return await asyncio.gather(*tasks)
+
+
+class G1Parser(AbstractNewsParser):
+    def ParseNews(self, rawData: dict) -> Generator[NewsInfo, None, None]:
+        for item in rawData["items"]:
+            date = item["created"].split("T")[0].split("-")
+            newsTime = datetime.date(int(date[0]), int(date[1]), int(date[2]))
+            today = datetime.date.today()
+            timeElapsed = today - newsTime
+
+            if timeElapsed.days > 1:
+                continue
+
+            yield NewsInfo(
+                title=item["content"]["title"],
+                summary=item["content"]["summary"],
+                url=item["content"]["url"],
+            )
