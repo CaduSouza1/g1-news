b6f3c7de3f1a17748f4878559ef0b2df6c926413
Carlos <cadusouza14@gmail.com>
2021-07-31 12:07:33 -0300

    Moved some fucntions to another file

--- mail.py
+++ mail.py
@@ -1,34 +1,4 @@
-import scrap
 from email.message import EmailMessage
-import jinja2
-
-
-def ParseNewsToEmailStr(
-    info: scrap.NewsInfo,
-    titleBlankLines: int = 1,
-    summaryBlankLines: int = 1,
-    urlBlankLines: int = 1,
-    styleFolderPath: str = None,
-    styleFilename: str = None,
-) -> str:
-    if styleFolderPath:
-        return (
-            jinja2.Environment(loader=jinja2.FileSystemLoader(styleFolderPath), autoescape=jinja2.select_autoescape())
-            .get_template(styleFilename)
-            .render(
-                title=info.title,
-                titleBlankLines=titleBlankLines,
-                summary=info.summary,
-                summaryBlankLines=summaryBlankLines,
-                url=info.url,
-                urlBlankLines=urlBlankLines,
-            )
-        )
-
-    return (
-        info.title + "\n" * titleBlankLines + info.summary + "\n" * summaryBlankLines + info.url + "\n" * urlBlankLines
-    )
-
 
 def CreateMessage(From: str, to: str, subject: str, content: str, styled: bool = False) -> EmailMessage:
     msg = EmailMessage()
