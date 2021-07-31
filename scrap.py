35b0813be19e10445262555ffd8b6d8e140b5ab2
Carlos <cadusouza14@gmail.com>
2021-07-31 12:06:50 -0300

    Added a way to categorize the news data

--- scrap.py
+++ scrap.py
@@ -1,38 +1,124 @@
 import abc
 import asyncio
+from collections import defaultdict
 import dataclasses
 import datetime
 import json
+import pathlib
 from typing import Generator, Iterable
 
 import aiohttp
+import jinja2
+import yarl
 from bs4 import BeautifulSoup
+
+# import enum
+
+
+# class Category(enum.IntEnum):
+#     EDUCATION = enum.auto()
+#     HEALTH = enum.auto()
+
+#     def ToStr(self) -> str:
+#         translationTable = {
+#             self.EDUCATION: "Education",
+#             self.HEALTH: "Health"
+#         }
+
+#         return translationTable[self.value]
 
 
 @dataclasses.dataclass
 class NewsInfo:
     title: str
     summary: str
-    url: str
+    url: yarl.URL
+    category: str
 
-    def ToEmailStr(self) -> str:
-        pass
+    def ToEmailStr(self, titleBlankLines: int, summaryBlankLines: int, urlBlankLines: int) -> str:
+        return (
+            self.title
+            + "\n" * titleBlankLines
+            + self.summary
+            + "\n" * summaryBlankLines
+            + self.url
+            + "\n" * urlBlankLines
+        )
+
+    def ToStyledEmailStr(
+        self, titleBlankLines: int, summaryBlankLines: int, urlBlankLines: int, styleFilepath: pathlib.Path
+    ) -> str:
+        return (
+            jinja2.Environment(
+                loader=jinja2.FileSystemLoader(styleFilepath.parent), autoescape=jinja2.select_autoescape()
+            )
+            .get_template(styleFilepath.name)
+            .render(
+                title=self.title,
+                titleBlankLines=titleBlankLines,
+                summary=self.summary,
+                summaryBlankLines=summaryBlankLines,
+                url=self.url,
+                urlBlankLines=urlBlankLines,
+            )
+        )
 
 
 @dataclasses.dataclass
 class NewsCollection:
-    categories: dict[str, list[NewsInfo]]  # {"education": [news1, news2, ...]}
+    news: list[NewsInfo]
 
-    def ToEmailStr(self) -> str:
-        pass
+    # def ToEmailStr(self, categorize: bool) -> str:
+    #     if categorize:
+    #         categorizedNews = self.Categorize()
 
-    def Styled(self) -> str:
-        pass
+    def ToStyledEmailStr(
+        self, titleBlankLines: int, summaryBlankLines: int, urlBlankLines: int, styleFilepath: pathlib.Path
+    ) -> str:
+        """Creates an html string based on a template defined at `styleFilepath`
+
+        Args:
+            titleBlankLines (int): Blank lines added at the end of the title
+            summaryBlankLines (int): Blank lines added at the end of the summary
+            urlBlankLines (int): Blank lines at the end of the url
+            styleFilepath (pathlib.Path): Path to the file containing the template
+
+        Returns:
+            str: Html string representation of the news objects in this collection
+        """
+
+        template = jinja2.Environment(
+            loader=jinja2.FileSystemLoader(styleFilepath.parent), autoescape=jinja2.select_autoescape()
+        ).get_template(styleFilepath.name)
+
+        return "".join(
+            map(
+                lambda newsCollection: template.render(
+                    category=newsCollection[0],
+                    newsCollection=newsCollection[1],
+                    titleBlankLines=titleBlankLines,
+                    summaryBlankLines=summaryBlankLines,
+                    urlBlankLines=urlBlankLines,
+                ),
+                self.Categorize().items(),
+            )
+        )
+
+    def Categorize(self) -> dict[str, list[NewsInfo]]:
+        categories = defaultdict(list)
+        for newsItem in self.news:
+            categories[newsItem.category].append(newsItem)
+
+        return categories
 
 
 class AbstractNewsScrapper(abc.ABC):
     @abc.abstractmethod
-    async def ScrapNews(self, session: aiohttp.ClientSession, urls: Iterable[str]) -> tuple[dict]:
+    def FilterInfo(self, info: str) -> str:
+        pass
+
+    @abc.abstractmethod
+    async def ScrapNews(self, session: aiohttp.ClientSession, urls: Iterable[yarl.URL]) -> tuple[dict]:
         pass
 
 
@@ -43,8 +129,15 @@ class AbstractNewsParser(abc.ABC):
 
 
 class G1Scrapper(AbstractNewsScrapper):
-    async def ScrapNews(self, session: aiohttp.ClientSession, urls: Iterable[str]) -> tuple[dict]:
-        async def G1News(url: str) -> dict:
+    def FilterInfo(self, info: str) -> str:
+        infoStart = info.find('"config":') - 1
+        infoEnd = info.rfind(", {lazy")
+
+        return info[infoStart:infoEnd]
+
+    async def ScrapNews(self, session: aiohttp.ClientSession, urls: Iterable[yarl.URL]) -> tuple[dict]:
+        async def G1News(url: yarl.URL) -> dict:
+            print(f"Requesting {url}")
             async with session.get(url) as response:
                 soup = BeautifulSoup(await response.text(), "html.parser")
 
@@ -55,12 +148,9 @@ class G1Scrapper(AbstractNewsScrapper):
 
                 info = newsGrid.find_all("div", recursive=False)[-1]
                 script = str(info.div.div.div.script)
-                jsonInfoStart = script.find('"config":') - 1
-                jsonInfoEnd = script.rfind(", {lazy")
 
-                newsRawData = script[jsonInfoStart:jsonInfoEnd]
-
-                return json.loads(newsRawData)
+                print(f"Request finished")
+                return json.loads(self.FilterInfo(script))
 
         tasks = (asyncio.create_task(G1News(url)) for url in urls)
 
@@ -68,7 +158,7 @@ class G1Scrapper(AbstractNewsScrapper):
 
 
 class G1Parser(AbstractNewsParser):
-    def ParseNews(self, rawData: dict) -> Generator[NewsInfo, None, None]:
+    def ParseNews(self, category: str, rawData: dict) -> Generator[NewsInfo, None, None]:
         for item in rawData["items"]:
             date = (
                 item["created"].split("T")[0].split("-")
@@ -77,11 +167,12 @@ class G1Parser(AbstractNewsParser):
             today = datetime.date.today()
             timeElapsed = today - newsTime
 
-            if timeElapsed.days > 1:
-                continue
+            # if timeElapsed.days > 1:
+            #     continue
 
             yield NewsInfo(
                 title=item["content"]["title"],
                 summary=item["content"]["summary"],
-                url=item["content"]["url"]
+                url=item["content"]["url"],
+                category=category,
             )
